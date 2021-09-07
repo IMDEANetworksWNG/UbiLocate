1829d6ba401a817391fb9ca76524cabb2ed2a94c
imdea <blancopizarro@gmail.com>
2021-09-07 09:16:07 +0200

    removing old files

--- rawperf/send_periodically/copy_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pwd
-cd "$(dirname "$0")"
-pwd
-bash copy_files_ind.sh 3
-bash copy_files_ind.sh 4
--- rawperf/send_periodically/disable_phase_jumps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd "$(dirname "$0")"
-
-./nexutil -I eth6 -s526 -i -l4 -v1
